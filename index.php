78903f45acd7fdad8c8e7b73e22ed4e18d25f4c8
AlenVarazdinac <alen.varazdinac@gmail.com>
2017-10-09 18:19:33 +0200

    Includes folder

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php 
-echo 'Hello world!';
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once 'includes/head-inc.php';?>
+</head>
+
+<body>
+    
+    <?php include_once 'includes/scripts-inc.php';?>
+</body>
+
+</html>
